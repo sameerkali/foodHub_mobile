82bab6a867ba83706c3c30d31b38c66e4f2da88f
Sameer Faridi <s.faridi007@gmail.com>
2023-12-22 21:21:52 +0530

    initial application

---
+++ app/(home)/index.js
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const index = () => {
+  return (
+    <View>
+      <Text>home hai home haihome haihome hai</Text>
+    </View>
+  )
+}
+
+export default index
+
+const styles = StyleSheet.create({})
